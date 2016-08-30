d9ea7819c5176a10ce0fdb6fa6d7c21c11c3634d
Roma-Za <zapriy.05@mail.ru>
2016-08-30 17:32:23 +0300

    Initial commit

---
+++ app/src/main/java/com/zapriy/roman/calculationofdominions/StartActivity.java
@@ -0,0 +1,22 @@
+package com.zapriy.roman.calculationofdominions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void newOnClick(View view) {
+    }
+    public void loadOnClick(View view) {
+    }
+    public void deleteOnClick(View view) {
+    }
+
+}
